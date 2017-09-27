0732db7e3d7b7685b5086010a17cd0795173fb47
asdasdwwyy <448187463@qq.com>
2017-09-27 14:29:42 +0800

    add swagger

--- src/main/java/com/uic/webbasederp/controller/HelloWorldController.java
+++ src/main/java/com/uic/webbasederp/controller/HelloWorldController.java
@@ -3,15 +3,18 @@ package com.uic.webbasederp.controller;
 import com.uic.webbasederp.domain.po.User;
 import com.uic.webbasederp.utilConst.CommonResultResponse;
 import com.uic.webbasederp.utilConst.ResponseHelper;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class HelloWorldController {
-    @RequestMapping("/hello")
+    @ApiOperation(value = "test")
+    @RequestMapping(value="/hello",method = RequestMethod.POST)
     public ResponseEntity<CommonResultResponse> index() throws Exception{
         User user = new User();
         user.setPassword("123456");
